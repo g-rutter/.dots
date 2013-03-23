144b1d82688e7ff467afc20046a1a275b83074be
Gil Rutter <g.o.rutter@warwick.ac.uk>
2013-03-23 22:24:25 +0000

    Create .ssh dir if it doesn't exist already.

--- create_symlinks.sh
+++ create_symlinks.sh
@@ -2,7 +2,11 @@
 
 ln  -sv  ~/.dots/profile     ~/.profile
 ln  -sv  ~/.dots/tmux.conf   ~/.tmux.conf
-ln  -sv  ~/.dots/ssh_config  ~/.ssh/config
 ln  -sv  ~/.dots/bash_ps1    ~/.bash_ps1
 ln  -sv  ~/.dots/vimrc       ~/.vimrc
 ln  -sv  ~/.dots/inputrc     ~/.inputrc
+
+echo ""
+
+mkdir -vp ~/.ssh
+ln  -sv  ~/.dots/ssh_config  ~/.ssh/config
