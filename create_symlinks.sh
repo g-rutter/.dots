dd9a6e83dfb04f0003ba6db2bcc2e2b3015c2ac6
Gil Rutter <g.o.rutter@warwick.ac.uk>
2013-03-24 00:57:01 +0000

    Added separate options for ln based on platform.

--- create_symlinks.sh
+++ create_symlinks.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 reset_style='\033[00m'
 yellow=$reset_style'\033[0;33m'
@@ -7,18 +7,29 @@ bold_white=$reset_style'\[\033[1;29m\]'
 bold_green=$reset_style'\[\033[1;32m\]'
 bold_magenta=$reset_style'\033[0;35m'
 
+PLATFORM=`uname -s | tr '[A-Z]' '[a-z]'`
+echo $PLATFORM
+
+if [ $PLATFORM == "mac" ]; then
+   ln_dir_options="-svFfh"
+elif [ $PLATFORM == "linux" ]; then
+   ln_dir_options="--symbolic -sfnv --symbolic"
+fi
+
 ln  -sv  ~/.dots/profile     ~/.profile
 ln  -sv  ~/.dots/tmux.conf   ~/.tmux.conf
 ln  -sv  ~/.dots/bash_ps1    ~/.bash_ps1
 ln  -sv  ~/.dots/inputrc     ~/.inputrc
 
-echo "$bold_magenta""\nInstalling ssh config file.""$reset_style"
+echo -e "$bold_magenta""\nInstalling ssh config file.""$reset_style"
 mkdir -v ~/.ssh
 ln  -sv  ~/.dots/ssh_config  ~/.ssh/config
 
-echo "$bold_magenta""\nInstalling .vimrc and bundles.""$reset_style"
-ln  -sv    ~/.dots/vimrc       ~/.vimrc
-ln  -svFfh ~/.dots/vim/bundle  ~/.vim/bundle
-ln  -svFfh ~/.dots/vim/colors  ~/.vim/colors
-ln  -svFfh ~/.dots/vim/Ultisnips  ~/.vim/Ultisnips
-echo "$bold_magenta""Now run :BundleInstall inside Vim.""$reset_style"
+echo -e "$bold_magenta""\nInstalling .vimrc and bundles.""$reset_style"
+mkdir -v     ~/.vim
+mkdir -v     ~/.vim/backup
+ln    -sv    ~/.dots/vimrc          ~/.vimrc
+ln    $ln_dir_options ~/.dots/vim/bundle     ~/.vim/bundle
+ln    $ln_dir_options ~/.dots/vim/colors     ~/.vim/colors
+ln    $ln_dir_options ~/.dots/vim/Ultisnips  ~/.vim/Ultisnips
+echo -e "$bold_magenta""Now run :BundleInstall inside Vim.""$reset_style"
